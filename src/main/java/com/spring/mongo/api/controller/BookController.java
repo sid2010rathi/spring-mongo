7ef863a0d1a14d36ab6894c709942697bf64214a
Siddharth Rathi <sid2010rathi@gmail.com>
2021-02-07 11:10:31 -0500

    Complete CRUD opration
    Add Update Operation

--- src/main/java/com/spring/mongo/api/controller/BookController.java
+++ src/main/java/com/spring/mongo/api/controller/BookController.java
@@ -4,10 +4,12 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -30,18 +32,28 @@ public class BookController {
 	}
 	
 	@GetMapping("/book")
-	public List<Book> retrieveBooks() {
+	public List<Book> getBooks() {
 		return bookRepository.findAll();
 	}
 	
 	@GetMapping("/book/{id}")
-	public Optional<Book> getBook(@PathVariable Integer id) {
+	public Optional<Book> getBook(@PathVariable(value = "id") Integer id) {
 		return bookRepository.findById(id);
 	}
 	
 	@DeleteMapping("/book/{id}")
-	public String deleteBook(@PathVariable Integer id) {
+	public String deleteBook(@PathVariable(value = "id") Integer id) {
 		bookRepository.deleteById(id);
 		return "Book is deleted";
+	}
+	
+	@PutMapping("/book/{id}")
+	public ResponseEntity<Book> updateBook(@PathVariable(value = "id") Integer id, @RequestBody Book book) {
+		Optional<Book> books = bookRepository.findById(id);
+		Book newBook = books.get();
+		newBook.setBookName(book.getBookName());
+		newBook.setAuthorName(book.getAuthorName());
+		Book updatedBook = bookRepository.save(newBook);
+		return ResponseEntity.ok(updatedBook);
 	}
 }
